4365a8d5978ccf85ada15dbebde0dae3f8e6c6f1
Sohel <shaikhsohel113441@gmail.com>
2025-03-05 12:43:40 +0530

    `Added socket.io-client dependency and updated package files to reflect the change`

--- public/main.js
+++ public/main.js
@@ -1,6 +1,8 @@
 // const socket = io();
+import { io } from "socket.io-client";
+const socket = io("https://chat-application-one-plum.vercel.app");
 
-const socket = io('https://chat-application-one-plum.vercel.app');
+// const socket = io('https://chat-application-one-plum.vercel.app');
 
 const clientsTotal = document.getElementById('clients-total');
 
